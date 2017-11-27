ffb5356dca046c14f07db3d85d597cc7c20a0632
Kelly turner <klynnturner@gmail.com>
2017-11-27 21:01:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logWhisper(string) {
+  console.log('hello')
+}
+function logShout(string) {
+  console.log('HELLO')
+}
+function sayHiToGrandma(string) {
+  string.toUpperCase() === 'HI, GRANDMA'
+  return "I can't hear you!"
+}
+function sayHiToGrandma(string) {
+  string.toLowerCase() === 'hi, grandma'
+  return "I love you, too."
+}
